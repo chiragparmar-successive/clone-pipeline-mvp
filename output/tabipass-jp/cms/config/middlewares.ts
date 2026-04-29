0cd02341f868bcf27af3a965577affec3233946f
Prabal555 <parbalraghav@Prabals-Macbook-Pro.local>
2026-04-29 18:40:41 +0530

    add cms generated file

---
+++ output/tabipass-jp/cms/config/middlewares.ts
@@ -0,0 +1,22 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+      headers: '*',
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
